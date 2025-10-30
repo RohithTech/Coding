c180fe5904547a70573b1a48ee448972dc63c77e
Rohith <Rohithkumar2196@gmail.com>
2025-10-30 17:45:06 +0530

    changes

--- simple.java
+++ simple.java
@@ -1,8 +1,8 @@
 package Coding;
 
-public class simple {
+class simple {
     // simple java code
-    public static void main(String[] args) {
+    public static void main(String[] args) {       
         System.out.println("Hello world!");
      }
     
